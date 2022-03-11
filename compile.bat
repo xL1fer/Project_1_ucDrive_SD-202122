9ecbd05b3dbca978f08e40110105c1ce4877839b
luisbatista <luis.m.g.batista@gmail.com>
2022-03-11 11:22:32 +0000

    added all java classes to compile.bat since subclasses were not being recompiled (they were not being explicitly called with a package for example)

--- compile.bat
+++ compile.bat
@@ -1,8 +1,8 @@
 cd UcDrive_Server
-javac UcDrive_Server.java
+javac UcDrive_Server.java ClientHandler.java User.java ClientAuth.java ServerUploadHandler.java ServerDownloadHandler.java
 cd ..
 cd Client
-javac Client.java
+javac Client.java ClientAuth.java ClientDownloadHandler.java ClientUploadHandler.java
 cd ..
 
 pause
